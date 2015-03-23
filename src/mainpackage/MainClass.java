d779c7814d7d8e2083f4020eaef45746c3419cbb
Heber <heber.im87@gmail.com>
2015-03-23 00:43:23 -0300

    Primer Commit

---
+++ src/mainpackage/MainClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainpackage;
+
+/**
+ *
+ * @author Heber
+ */
+public class MainClass {
+    // GIT!
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.setVisible(true);
+    }
+    
+}
